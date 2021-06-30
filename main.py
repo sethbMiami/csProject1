c59cc3962d47da1bb49061c6364069d8f18514ec
seth bruner <bruner.seth@outlook.com>
2021-06-30 12:05:07 -0400

    initial commit of program

---
+++ main.py
@@ -0,0 +1,36 @@
+# this is the main program for Codecadmy CS Project - Mortgage calculator.
+
+p = 0 # this is the principal amount of the house
+dp = 0 # this is the down payment amount towards the principal amount
+r = 4.5 # annual interest rate 4.5%
+n = 360 # number of monthly payments for a 30 year mortgage
+apt = 2500
+api = 1500
+
+def calculate_mortgage_m(p, dp):    # function calculates monthly mortgage payment
+    np = p - dp
+    i = (r/100)/12 
+    # M = P [ i(1 + i)^n ] / [ (1 + i)^n – 1] mortgage payment formula
+    m = np * (i(1+i)**n)/((1+i)**n - 1)
+    monthly_payment = m + (apt / 12) + (api / 12)
+    return monthly_payment
+
+def yes_or_no(question):
+    while "the answer is invalid":
+        reply = str(input(question+' (y/n): ')).lower().strip()
+        if reply[0] == 'y':
+            return True
+        if reply[0] == 'n':
+            return False
+
+print("Welcome to the CS project Mortgage Calculator!")
+print("This program will calculate your monthly mortgage payment based off of the house principal amount and your initial downpayment")
+print("*note - This monthly payment is based off of a 30 year fixed mortgage and includes the annual interest rate of 4.5%, with an annual property tax of $2500 and annual property insurance of $1550")
+
+yes_or_no("Do you wish to proceed?")
+
+if yes_or_no == True:
+    print("Lets get started!")
+    p = input("What is the price of the home?")
+    
+
